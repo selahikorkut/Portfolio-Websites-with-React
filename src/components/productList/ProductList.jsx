f52eeead448c59251ff54d08182c25873441dd18
Selahi KORKUT <37619156+selahikorkut@users.noreply.github.com>
2022-04-19 02:52:58 +0300

    Add files via upload

---
+++ src/components/productList/ProductList.jsx
@@ -0,0 +1,23 @@
+import "./productList.css";
+import Product from "../product/Product";
+import { products } from "../../data";
+
+const ProductList = () => {
+  return (
+    <div className="pl">
+      <div className="pl-texts">
+        <h1 className="pl-title">Create & inspire. It's Selo</h1>
+        <p className="pl-desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non pariatur, laborum quo consectetur in porro nam libero ab earum facere possimus, voluptatem dicta voluptates atque.
+        </p>
+      </div>
+      <div className="pl-list">
+        {products.map((item) => (
+          <Product key={item.id} img={item.img} link={item.link} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    link: "https://www.linkedin.com/in/selahi-korkut/",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/35550/ipad-tablet-technology-touch.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    link: "https://www.linkedin.com/in/selahi-korkut/",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/147413/twitter-facebook-together-exchange-of-information-147413.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    link: "https://www.linkedin.com/in/selahi-korkut/",
+  },
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    link: "https://www.linkedin.com/in/selahi-korkut/",
+  },
+  {
+    id: 5,
+    img: "https://images.pexels.com/photos/1356300/pexels-photo-1356300.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    link: "https://www.linkedin.com/in/selahi-korkut/",
+  },
+  {
+    id: 6,
+    img: "https://images.pexels.com/photos/331788/pexels-photo-331788.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    link: "https://www.linkedin.com/in/selahi-korkut/",
+  },
+];
